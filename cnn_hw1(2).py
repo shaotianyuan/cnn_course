9ccf60ab7a8e3f8900da0849ab22db770f674723
shaotianyuan <934491893@qq.com>
2020-01-08 09:44:39 +0800

    hw1(2)updata

--- cnn_hw1(2).py
+++ cnn_hw1(2).py
@@ -2,29 +2,22 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('homework.jpeg')
-
 def my_show(i):
     plt.imshow(cv2.cvtColor(i, cv2.COLOR_BGR2RGB))
     plt.show()
 
+img = cv2.imread('homework.jpeg')
+
 bg_color = [188,129,36]
-threshold = 8500
 
+B, G, R = cv2.split(img)
+B = B.astype('uint32')
+G = G.astype('uint32')
+R = R.astype('uint32')
 
-def calc_diff(pixel):
-    return (pixel[0]-bg_color[0])**2 + (pixel[1]-bg_color[1])**2 + (pixel[2]-bg_color[2])**2
+temp = (B - bg_color[0]) ** 2 + (G - bg_color[1]) ** 2 + (R - bg_color[2]) ** 2
+img[temp < 8500] = 255
 
-def background(picture):
-    h, w = picture.shape[0:2]
-    for i in range(h):
-        for j in range(w):
-            if calc_diff(picture[i][j]) < threshold:
-                picture[i][j][0] = 255
-                picture[i][j][1] = 255
-                picture[i][j][2] = 255
-
-background(img)
 my_show(img)
 
 cv2.imwrite('result.png',img)
